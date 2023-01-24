02e2a4c08b4ac4e75eb81b093e36155dfce03efb
Joseth222 <107093649+joseth22@users.noreply.github.com>
2023-01-24 17:17:21 -0600

    creación del proyecto

---
+++ src/main/java/com/Tienda_IQ23/TiendaIq23Application.java
@@ -0,0 +1,13 @@
+package com.Tienda_IQ23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIq23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIq23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_IQ23/TiendaIq23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_IQ23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIq23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
